d516278a4db7e0ccb6e2e0b7b09e1d4bc2b06276
Jeffrey Sun <js66@illinois.edu>
2020-12-03 15:31:49 -0600

    create hello world with input

---
+++ chapter-03/hello_world_input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string first_name;
+    cout << "Please enter your first name (followed by 'enter'): ";
+    cin >> first_name;
+    cout << "Hello, " << first_name << "!\n";
+}
