2a15d0a95b13f2c11fbeec6c7b1869d9a499332b
Jeffrey Sun <js66@illinois.edu>
2020-12-03 16:30:08 -0600

    drill step 1

---
+++ chapter-03/drill.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string first_name;
+    cout << "Please enter your first name: ";
+    cin >> first_name;
+    cout << "Hello, " << first_name << endl;
+}
