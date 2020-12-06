d234b493df91546e1750ce4d02aee5ffef3af5b7
Jeffrey Sun <js66@illinois.edu>
2020-12-05 21:40:40 -0600

    attempt step 07

--- chapter-04/drill.cpp
+++ chapter-04/drill.cpp
@@ -3,27 +3,68 @@
 using namespace std;
 
 int main() {
-    cout << "Please enter pairs of values or a '|' to exit the program: ";
+    const double m_to_cm = 100.0;
+    const double in_to_cm = 2.54;
+    const double ft_to_cm = 12 * in_to_cm;
+
+    cout << "Please enter a value followed by a unit or a '|' to exit the program: ";
     double smallest;
     double largest;
     double value;
-    if (cin >> value && value != '|') {
-        smallest = value;
-        largest = value;
-        cout << value << endl;
+    string unit;
+    if (cin >> value >> unit && value != '|') {
+        if (unit == "cm") {
+            smallest = value;
+            largest = value;
+        } else if (unit == "m") {
+            smallest = m_to_cm * value;
+            largest = m_to_cm * value;
+        } else if (unit == "in") {
+            smallest = in_to_cm * value;
+            largest = in_to_cm * value;
+        } else if (unit == "ft") {
+            smallest = ft_to_cm * value;
+            largest = ft_to_cm * value;
+        }
+        cout << value << " " << unit << endl;
     }
-    while (cin >> value) {
+    while (cin >> value >> unit) {
+        cout << value << " " << unit << endl;
+        char id;
+        double cm_value;
         if (value == '|') {
             break;
         }
-        cout << value << endl;
-        if (value < smallest) {
-            smallest = value;
-            cout << "The smallest so far" << endl;
+        if (unit == "cm") {
+            char id = 'c';
+        } else if (unit == "m") {
+            char id = 'm';
+        } else if (unit == "in") {
+            char id = 'i';
+        } else if (unit == "ft") {
+            char id = 'f';
         }
-        if (value > largest) {
-            largest = value;
+        switch (id) {
+            case 'c':
+                cm_value = value;
+                break;
+            case 'm':
+                cm_value = m_to_cm * value;
+                break;
+            case 'i':
+                cm_value = in_to_cm * value;
+                break;
+            case 'f':
+                cm_value = ft_to_cm * value;
+                break;
+        }
+        if (cm_value > largest) {
+            largest = cm_value;
             cout << "The largest so far" << endl;
+        }
+        if (cm_value < smallest) {
+            smallest = cm_value;
+            cout << "The smallest so far" << endl;
         }
     }
     return 0;
