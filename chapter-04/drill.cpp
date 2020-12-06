a25b126a87fea1177cbb538640e4fa2efdc1e1de
Jeffrey Sun <js66@illinois.edu>
2020-12-05 18:58:46 -0600

    completed drill step 04 and 05

--- chapter-04/drill.cpp
+++ chapter-04/drill.cpp
@@ -4,14 +4,27 @@ using namespace std;
 
 int main() {
     cout << "Please enter pairs of values or a '|' to exit the program: ";
-    int val1;
-    int val2;
+    double val1;
+    double val2;
     while (cin >> val1 && cin >> val2) {
         if (val1 == '|' || val2 == '|') {
             break;
         }
-        cout << val1 << endl;
-        cout << val2 << endl;
+        if (val1 > val2) {
+            cout << "The smaller value is: " << val2 << endl;
+            cout << "The larger value is: " << val1 << endl;
+            if (val1 - val2 < 0.01) {
+                cout << "The numbers are almost equal" << endl;
+            }
+        } else if (val2 > val1) {
+            cout << "The smaller value is: " << val1 << endl;
+            cout << "The larger value is: " << val2 << endl;
+            if (val2 - val1 < 0.01) {
+                cout << "The numbers are almost equal" << endl;
+            }
+        } else {
+            cout << "The numbers are equal" << endl;
+        }
     }
     return 0;
 }
