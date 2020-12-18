5c92516a2e93b953a497088bfa8cef84d53802b5
Jeffrey Sun <js66@illinois.edu>
2020-12-18 17:21:43 -0600

    test inputs

--- chapter-04/test.cpp
+++ chapter-04/test.cpp
@@ -1,8 +1,13 @@
-include <iostream>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    
-    return 0;
+    double num = 0;
+    string str = "";
+    cin >> num >> str; // cin stops searching for int after nondigit
+    cout << "string is : " << str << endl;
+    cout << "number is: " << num << endl;
+
+    // return 0 at the end is unnecessary
 }
