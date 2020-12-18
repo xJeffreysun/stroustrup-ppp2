c925f43b8c5904af1b2800f3f586b8b56f3e58b9
Jeffrey Sun <js66@illinois.edu>
2020-12-18 17:34:22 -0600

    step 01

--- chapter-04/drill.cpp
+++ chapter-04/drill.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 int main() {
-    
+    int first;
+    int second;
+
+    cout << "Read in two ints: ";
+    while (cin >> first >> second) {
+        if (first == '|' || second == '|') {
+            return 0;
+        }
+        cout << "First of the pair was: " << first << endl;
+        cout << "Second of the pair was: " << second << endl;
+    }
 
     // return 0 at the end is unnecessary
 }
